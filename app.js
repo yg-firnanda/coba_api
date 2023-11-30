42859e62e648a1965c763e22e5db2c91281a6308
Mochammad Yoga Firnanda <mochyoga475@gmail.com>
2023-11-30 19:00:16 +0700

    update

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send("Hello WKWKWK");
 })
 
+app.get('/about', (req, res) => {
+    res.send("Tentang saya");
+})
+
 app.listen(3000, () => {
     console.log("Successful!");
 })
